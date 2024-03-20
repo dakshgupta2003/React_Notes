2a9e66b66d44fd798194616c6acc2ffcd82c5bf0
Daksh Gupta <dakshmanu2003@gmail.com>
2024-03-20 15:49:46 +0530

    states

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Textform from "./components/Textform";
 function App() {
   return (
     <>
-      
-      <Navbar title="TextUtils" about_text="about us"/>
+      <Navbar title="TextUtils" about_text="about us" />
+      {/* container is a class in BOOTSTRAP that gives the element inside it some css properties */}
+      {/* my-3 is a property in bootstrap that give margin y of 3px */}
+      <div className="container my-3">
+        <Textform heading ="Enter the Text to Analyze" />
+      </div>
     </>
   );
 }
